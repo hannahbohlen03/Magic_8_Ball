2920b2c1aa75f653788a82ff88b1f8bd7d419e80
Hannah <hannah.bohlen.2021@student.lhschools.org>
2019-02-07 08:28:33 -0500

    Declared and Started Initializing

--- src/Magic_8_Ball.java
+++ src/Magic_8_Ball.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 public class Magic_8_Ball {
     public static void main(String[]args){
 
-        
+        //Declare
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+        int testing;
 
-
+        //Initializing
+        System.out.println("Pick a number between 1 and 100.");
+        testing = keyboard.nextInt();
 
 
 
